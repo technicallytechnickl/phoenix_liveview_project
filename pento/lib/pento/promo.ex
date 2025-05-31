250932b43ea44d6d02e979ff01587d34b539f54c
technicallytechnickl <31936413+technicallytechnickl@users.noreply.github.com>
2025-05-31 14:43:19 -0400

    completed chapter (#2)
    Co-authored-by: tech_nickl <tech_nickl@Mac>

--- pento/lib/pento/promo.ex
+++ pento/lib/pento/promo.ex
@@ -1,13 +1,16 @@
 defmodule Pento.Promo do
   alias Pento.Promo.Recipient
+  alias Pento.Accounts.UserNotifier
 
   def change_recipient(%Recipient{} = recipient, attrs \\ %{}) do
     Recipient.changeset(recipient, attrs)
   end
 
-  def send_promo(_recipient, _attrs) do
+  def send_promo(recipient) do
     # send email to promo recipient
     # exercise for the reader
+    IO.inspect(recipient)
+    UserNotifier.deliver_promotion(recipient["recipient"]["email"])
     {:ok, %Recipient{}}
   end
 end
